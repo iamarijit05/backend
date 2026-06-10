39429bad6d1e73f78cf039e58e412febd83686af
ARIJIT DAS <iarijitdas03@gmail.com>
2026-06-10 12:39:37 +0530

    cloudinary setup and multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,31 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+
+
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+});
+
+const uploadonCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null;
+        //upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        //file has been uploaded successfully
+        console.log("File is Uploaded on cloudinary", response.url)
+        return response;
+    } catch (error) {
+        //remove the locally saved temporary file as the upload op got failed
+        fs.unlinkSync(localFilePath);
+        return null;
+    }
+}
+
+
+export {uploadonCloudinary}
